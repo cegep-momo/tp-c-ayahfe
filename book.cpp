f0b1a3786af4431725757ecc741c4c8f6437b180
ahafiane <ahafiane44@gmail.com>
2025-10-26 18:44:07 -0400

    Ajout des setters sur book.cpp

--- book.cpp
+++ book.cpp
@@ -7,3 +7,10 @@ using namespace std;
 Book::Book() : title(""), author(""), isbn(""), isAvailable(true), borrowerName("") {}
 Book::Book(const std::string& t, const std::string& a, const std::string& i)
     : title(t), author(a), isbn(i), isAvailable(true), borrowerName("") {}
+
+//Getters
+std::string Book::getTitle()const {return title;}
+std::string Book::getAuthor()const {return author;}
+std::string Book::getISBN() const {return isbn;}
+bool Book::getAvailability() const {return isAvailable;}
+std::string Book::getBorrowerName() const {return borrowerName;}
